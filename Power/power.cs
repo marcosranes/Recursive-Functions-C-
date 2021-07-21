7e7fd4ca7607d4d4ade9b79c226f5cdc578d72ff
Marcos Ranes <marcosranes@gmail.com>
2021-07-21 19:10:58 -0300

    fixing inconsistency of the results, changing to double type

--- Power/power.cs
+++ Power/power.cs
@@ -1,20 +1,19 @@
 using System;
 class Power{
   static void Main(string[] args){
-    long number1, number2, result;
-    double res;
+    double number1, number2, result, res;
     Console.Write("Type the BASE number: ");
-    number1 = Convert.ToInt32(Console.ReadLine());
+    number1 = Convert.ToDouble(Console.ReadLine());
     Console.Write("Define its EXPONENT: ");
-    number2 = Convert.ToInt32(Console.ReadLine());
+    number2 = Convert.ToDouble(Console.ReadLine());
     result = power(number1, number2);
     Console.WriteLine("Recursive SOLUTION: {0} ^ {1} = {2}", number1, number2, result);
     res = Math.Pow(Convert.ToDouble(number1), Convert.ToDouble(number2));
     Console.WriteLine("Proof of Work: Math.Pow({0}, {1}) = {2}", number1, number2, res);
-
+    
     exit("\npress any key to exit the process...");
   }
-  static long power(long n1, long n2){
+  static double power(double n1, double n2){
     if(n2 == 0){
       return 1;
     }
